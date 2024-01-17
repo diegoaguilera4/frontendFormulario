64f83c0e6274176f911db60fb1c2baacd2d0d34b
Diego Aguilera <diegoaguilera4@icloud.com>
2024-01-17 09:33:42 -0300

    se crea archivo en utils para crearPdf en diferentes vistas

---
+++ src/utils/crearPdf.js
@@ -0,0 +1,120 @@
+import JsBarcode from "jsbarcode";
+
+export function generarPdf(data) {
+    const pdfDefinition = {
+      content: [
+        {
+          text: "                                                                                                                              ",
+          style: "header",
+          alignment: "center",
+        },
+        {
+          text: "                                          CONTROL DE DESPERDICIO                                          ",
+          style: "header",
+          alignment: "center",
+        },
+        {
+          text: "                                                                                                                              ",
+          style: "header",
+          alignment: "center",
+        },
+        {
+          text: "\nRevisión N°: X\n",
+          style: "subheader",
+          alignment: "center",
+        },
+        {
+          style: "tableExample",
+          table: {
+            widths: ["*", "*"],
+            body: [
+              ["Área", data.area],
+              ["Fecha", new Date().toLocaleDateString()],
+              ["Turno", data.turno],
+              ["Responsable del rechazo", data.responsable],
+            ],
+          },
+        },
+      ],
+      styles: {
+        tableExample: {
+          margin: [0, 5, 0, 15],
+        },
+        header: {
+          fontSize: 20,
+          bold: true,
+          background: "#FF0000", // Fondo rojo
+          color: "#FFFFFF", // Color de texto blanco
+        },
+      },
+    };
+
+    // Verificar si hay defecto en lámina
+    if (data.defectoEnLamina && data.defectoEnLamina !== "Ningún defecto") {
+      pdfDefinition.content.push({
+        style: "tableExample",
+        table: {
+          widths: ["*", "*"],
+          body: [
+            ["Defecto en lámina", data.defectoEnLamina],
+            ["Causa de defecto en lámina", data.causaLamina],
+          ],
+        },
+      });
+    }
+
+    // Verificar si hay defecto en caja
+    if (data.defectoEnCaja && data.defectoEnCaja !== "Ningún defecto") {
+      pdfDefinition.content.push({
+        style: "tableExample",
+        table: {
+          widths: ["*", "*"],
+          body: [
+            ["Defecto en caja", data.defectoEnCaja],
+            ["Causa de defecto en caja", data.causaCaja],
+          ],
+        },
+      });
+    }
+
+    pdfDefinition.content.push(
+      {
+        style: "tableExample",
+        table: {
+          widths: ["*", "*"],
+          body: [
+            ["Operador picadora", data.operadorPicadora],
+            ["Cliente", data.cliente],
+            ["Producto", data.producto],
+            ["Cantidad", data.cantidad],
+            ["Nro OP", data.nroOp],
+          ],
+        },
+      },
+      {
+        style: "tableExample",
+        table: {
+          widths: ["*", "*"],
+          body: [
+            ["Autoriza picar", data.autorizaPicar],
+            ["Total kilos", data.totalKilos],
+          ],
+        },
+      }
+    );
+
+    const canvas = document.createElement("canvas");
+    JsBarcode(canvas, data._id, { format: "CODE128" });
+    // Redimensionar el canvas
+
+    // Convertir el canvas a imagen y agregarla al documento PDF
+    const imageData = canvas.toDataURL("image/png");
+    pdfDefinition.content.push({
+      image: imageData,
+      alignment: "center",
+      width: 300,
+    });
+
+    return pdfDefinition;
+  }
+
--- src/views/FormularioControl.vue
+++ src/views/FormularioControl.vue
@@ -179,7 +179,7 @@
 import axios from "axios";
 import pdfMake from "pdfmake/build/pdfmake";
 import pdfFonts from "pdfmake/build/vfs_fonts";
-import JsBarcode from 'jsbarcode';
+import { generarPdf } from "../utils/crearPdf";
 
 // Carga las fuentes necesarias para pdfmake
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
@@ -433,7 +433,8 @@ export default {
 
         if (res.status === 201) {
           // Generar el PDF
-          const pdfDefinition = this.generarPdf(res.data);
+
+          const pdfDefinition = generarPdf(res.data);
 
           // Abre el PDF en una nueva ventana o descárgalo
           pdfMake.createPdf(pdfDefinition).open();
@@ -452,114 +453,6 @@ export default {
       } finally {
         this.mostrarConfirmacion = false; // Cerrar la alerta después de enviar
       }
-    },
-    generarPdf(data) {
-      const pdfDefinition = {
-        content: [
-          {
-            text: "                                                                                                                              ",
-            style: "header",
-            alignment: "center",
-          },
-          {
-            text: "                                          CONTROL DE DESPERDICIO                                          ",
-            style: "header",
-            alignment: "center",
-          },
-          {
-            text: "                                                                                                                              ",
-            style: "header",
-            alignment: "center",
-          },
-          { text: "\nRevisión N°: X\n", style: "subheader", alignment: "center" },
-          {
-            style: "tableExample",
-            table: {
-              widths: ["*", "*"],
-              body: [
-                ["Área", data.area],
-                ["Fecha", new Date().toLocaleDateString()],
-                ["Turno", data.turno],
-                ["Responsable del rechazo", data.responsable],
-              ],
-            },
-          },
-        ],
-        styles: {
-          tableExample: {
-            margin: [0, 5, 0, 15],
-          },
-          header: {
-            fontSize: 20,
-            bold: true,
-            background: "#FF0000", // Fondo rojo
-            color: "#FFFFFF", // Color de texto blanco
-          },
-        },
-      };
-
-      // Verificar si hay defecto en lámina
-      if (data.defectoEnLamina && data.defectoEnLamina !== "Ningún defecto") {
-        pdfDefinition.content.push({
-          style: "tableExample",
-          table: {
-            widths: ["*", "*"],
-            body: [
-              ["Defecto en lámina", data.defectoEnLamina],
-              ["Causa de defecto en lámina", data.causaLamina],
-            ],
-          },
-        });
-      }
-
-      // Verificar si hay defecto en caja
-      if (data.defectoEnCaja && data.defectoEnCaja !== "Ningún defecto") {
-        pdfDefinition.content.push({
-          style: "tableExample",
-          table: {
-            widths: ["*", "*"],
-            body: [
-              ["Defecto en caja", data.defectoEnCaja],
-              ["Causa de defecto en caja", data.causaCaja],
-            ],
-          },
-        });
-      }
-
-      pdfDefinition.content.push(
-        {
-          style: "tableExample",
-          table: {
-            widths: ["*", "*"],
-            body: [
-              ["Operador picadora", data.operadorPicadora],
-              ["Cliente", data.cliente],
-              ["Producto", data.producto],
-              ["Cantidad", data.cantidad],
-              ["Nro OP", data.nroOp],
-            ],
-          },
-        },
-        {
-          style: "tableExample",
-          table: {
-            widths: ["*", "*"],
-            body: [
-              ["Autoriza picar", data.autorizaPicar],
-              ["Total kilos", data.totalKilos],
-            ],
-          },
-        }
-      );
-
-      const canvas = document.createElement('canvas');
-      JsBarcode(canvas, data._id, { format: 'CODE128' });
-
-      // Convertir el canvas a imagen y agregarla al documento PDF
-      const imageData = canvas.toDataURL('image/png');
-      pdfDefinition.content.push({ image: imageData, alignment: "center" ,width: 300});
-
-      return pdfDefinition;
     },
     mostrarAlerta() {
       this.mostrarConfirmacion = true; // Mostrar la alerta al hacer clic en "Enviar control"
