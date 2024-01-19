87b8a040cb088343c486b1c829fe031c4ef3731e
Diego Aguilera <diegoaguilera4@icloud.com>
2024-01-19 09:30:08 -0300

    mejoras visuales en el pdf generado

--- src/utils/crearPdf.js
+++ src/utils/crearPdf.js
@@ -1,126 +1,144 @@
 import JsBarcode from "jsbarcode";
 
 export function generarPdf(data) {
-    const pdfDefinition = {
-      content: [
-        {
-          text: "                                                                                                                              ",
-          style: "header",
-          alignment: "center",
-        },
-        {
-          text: "                                          CONTROL DE DESPERDICIO                                          ",
-          style: "header",
-          alignment: "center",
-        },
-        {
-          text: "                                                                                                                              ",
-          style: "header",
-          alignment: "center",
-        },
-        {
-          text: "\nRevisión N°: "+data.nroRevision+"\n",
-          style: "subheader",
-          alignment: "center",
-        },
-        {
-          style: "tableExample",
-          table: {
-            widths: ["*", "*"],
-            body: [
-              ["Área", data.area==="Otra" ? data.areaOtra+" ("+data.area+")" : data.area],
-              ["Fecha", data.fecha],
-              ["Turno", data.turno],
-              ["Responsable del rechazo", data.responsable],
+  const pdfDefinition = {
+    content: [
+      {
+        text: "                                                                                                                              ",
+        style: "header",
+        alignment: "center",
+      },
+      {
+        text: "                                          CONTROL DE DESPERDICIO                                          ",
+        style: "header",
+        alignment: "center",
+      },
+      {
+        text: "                                                                                                                              ",
+        style: "header",
+        alignment: "center",
+      },
+      {
+        text: "\nRevisión N°: " + data.nroRevision + "\n",
+        style: "subheader",
+        alignment: "center",
+      },
+      {
+        style: "tableExample",
+        table: {
+          widths: ["*", "*"],
+          body: [
+            [
+              { text: "Área", style: "tableHeader" },
+              data.area === "Otra"
+                ? data.areaOtra + " (" + data.area + ")"
+                : data.area,
             ],
-          },
-        },
-      ],
-      styles: {
-        tableExample: {
-          margin: [0, 5, 0, 15],
-        },
-        header: {
-          fontSize: 20,
-          bold: true,
-          background: "#D90000", // Fondo rojo
-          color: "#FFFFFF", // Color de texto blanco
+            [{ text: "Fecha", style: "tableHeader" }, data.fecha],
+            [{ text: "Turno", style: "tableHeader" }, data.turno],
+            [
+              { text: "Responsable del rechazo", style: "tableHeader" },
+              data.responsable,
+            ],
+          ],
         },
       },
-    };
+    ],
+    styles: {
+      tableExample: {
+        margin: [0, 5, 0, 15],
+      },
+      header: {
+        fontSize: 20,
+        bold: true,
+        background: "#D90000", // Fondo rojo
+        color: "#FFFFFF", // Color de texto blanco
+      },
+      tableHeader: {
+        bold: true,
+      },
+    },
+  };
 
-
-    // Verificar si hay defecto en lámina
-    if (data.defectoEnLamina && data.defectoEnLamina !== "Ningún defecto") {
-      pdfDefinition.content.push({
-        style: "tableExample",
-        table: {
-          widths: ["*", "*"],
-          body: [
-            ["Defecto en lámina", data.defectoEnLamina==="Otros" ? data.defectoEnLaminaOtros+" ("+data.defectoEnLamina+")" : data.defectoEnLamina],
-            ["Causa de defecto en lámina", data.causaLamina],
-          ],
-        },
-      });
-    }
-
-    // Verificar si hay defecto en caja
-    if (data.defectoEnCaja && data.defectoEnCaja !== "Ningún defecto") {
-      pdfDefinition.content.push({
-        style: "tableExample",
-        table: {
-          widths: ["*", "*"],
-          body: [
-            ["Defecto en caja", data.defectoEnCaja==="Otros" ? data.defectoEnCajaOtros+" ("+data.defectoEnCaja+")" : data.defectoEnCaja],
-            ["Causa de defecto en caja", data.causaCaja],
-          ],
-        },
-      });
-    }
-
-    pdfDefinition.content.push(
-        {
-          style: "tableExample",
-          table: {
-            widths: ["*", "*"],
-            body: [
-              ["N° Op", data.nroOp],
-              ["EstNumber", data.estNumber],
-              ["Producto", data.producto],
-              ["Cliente", data.cliente],
-              ["Cantidad", data.cantidad],
-              
-            ],
-          },
-        },
-    )
-
-    if(data.totalKilos){
-      pdfDefinition.content.push(
-        {
-          style: "tableExample",
-          table: {
-            widths: ["*", "*"],
-            body: [
-              ["Total kilos", data.totalKilos],
-            ],
-          },
-        }
-      );
-    }
-
-    const canvas = document.createElement("canvas");
-    JsBarcode(canvas, data._id, { format: "CODE128" });
-    // Redimensionar el canvas
-
-    // Convertir el canvas a imagen y agregarla al documento PDF
-    const imageData = canvas.toDataURL("image/png");
+  // Verificar si hay defecto en lámina
+  if (data.defectoEnLamina && data.defectoEnLamina !== "Ningún defecto") {
     pdfDefinition.content.push({
-      image: imageData,
-      alignment: "center",
-      width: 300,
+      style: "tableExample",
+      table: {
+        widths: ["*", "*"],
+        body: [
+          [
+            { text: "Defecto en lámina", style: "tableHeader" },
+            data.defectoEnLamina === "Otros"
+              ? data.defectoEnLaminaOtros + " (" + data.defectoEnLamina + ")"
+              : data.defectoEnLamina,
+          ],
+          [
+            { text: "Causa de defecto en lámina", style: "tableHeader" },
+            data.causaLamina,
+          ],
+        ],
+      },
     });
-
-    return pdfDefinition;
   }
 
+  // Verificar si hay defecto en caja
+  if (data.defectoEnCaja && data.defectoEnCaja !== "Ningún defecto") {
+    pdfDefinition.content.push({
+      style: "tableExample",
+      table: {
+        widths: ["*", "*"],
+        body: [
+          [
+            { text: "Defecto en caja", style: "tableHeader" },
+            data.defectoEnCaja === "Otros"
+              ? data.defectoEnCajaOtros + " (" + data.defectoEnCaja + ")"
+              : data.defectoEnCaja,
+          ],
+          [
+            { text: "Causa de defecto en caja", style: "tableHeader" },
+            data.causaCaja,
+          ],
+        ],
+      },
+    });
+  }
+
+  pdfDefinition.content.push({
+    style: "tableExample",
+    table: {
+      widths: ["*", "*"],
+      body: [
+        [{ text: "N° Op", style: "tableHeader" }, data.nroOp],
+        [{ text: "EstNumber", style: "tableHeader" }, data.estNumber],
+        [{ text: "Producto", style: "tableHeader" }, data.producto],
+        [{ text: "Cliente", style: "tableHeader" }, data.cliente],
+        [{ text: "Cantidad", style: "tableHeader" }, data.cantidad],
+      ],
+    },
+  });
+
+  if (data.totalKilos) {
+    pdfDefinition.content.push({
+      style: "tableExample",
+      table: {
+        widths: ["*", "*"],
+        body: [["Total kilos", data.totalKilos]],
+      },
+    });
+  }
+
+  const canvas = document.createElement("canvas");
+  JsBarcode(canvas, data._id, { format: "CODE128" });
+  // Redimensionar el canvas
+
+  // Convertir el canvas a imagen y agregarla al documento PDF
+  const imageData = canvas.toDataURL("image/png");
+  pdfDefinition.content.push({
+    image: imageData,
+    alignment: "center",
+    width: 300,
+  });
+
+  return pdfDefinition;
+}
