ea48ec8a8ba3dfd80a94766f481c18f2de942969
Diego Aguilera <diegoaguilera4@icloud.com>
2024-01-22 11:31:27 -0300

    mejoras de diseño en calendario

--- src/components/TablaForm.vue
+++ src/components/TablaForm.vue
@@ -7,7 +7,7 @@
         solo-inverted
         clearable
         @click:clear="clearSearch"
-        append-inner-icon="mdi-magnify"
+        append-inner-icon="mdi-magnify" 
       ></v-text-field>
     </v-col>
     <v-col>
@@ -18,6 +18,7 @@
         label="Filtrar por área"
         :items="getUniqueValues('area')"
         append-inner-icon="mdi-domain"
+        height="20"
       ></v-select>
     </v-col>
     <v-col>
@@ -41,7 +42,7 @@
       ></v-select>
     </v-col>
 
-    <v-col>
+    <v-col style="padding-top: 22px;">
       <VueDatePicker  
         v-model="filtroFecha"
         locale="es"
@@ -52,6 +53,7 @@
         range
         placeholder="Filtrar por rango de fechas"
         :partial-range="true"
+        
       />
     </v-col>
   </v-row>
@@ -444,6 +446,12 @@ export default {
 </script>
 
 <style>
+.v-text-field--box .v-input__slot,
+.v-text-field--outline .v-input__slot {
+  min-height: auto!important;
+  display: flex!important;
+  align-items: center!important;
+}
 .dp__theme_light {
   --dp-background-color: #f5f5f5;
   --dp-text-color: #000000;
@@ -474,45 +482,44 @@ export default {
 }
 
 :root {
-  /*General*/
-  --dp-font-family: -apple-system, blinkmacsystemfont, "Segoe UI", roboto,
-    oxygen, ubuntu, cantarell, "Open Sans", "Helvetica Neue", sans-serif;
-  --dp-border-radius: 0px; /*Configurable border-radius*/
-  --dp-cell-border-radius: 10px; /*Specific border radius for the calendar cell*/
-  --dp-common-transition: all 0.1s ease-in; /*Generic transition applied on buttons and calendar cells*/
+  /* General */
+  --dp-font-family: roboto;
+  --dp-border-radius: 0px; /* Radio de borde configurable */
+  --dp-cell-border-radius: 10px; /* Radio de borde específico para la celda del calendario */
+  --dp-common-transition: all 0.1s ease-in; /* Transición genérica aplicada a botones y celdas del calendario */
 
-  /*Sizing*/
-  --dp-button-height: 35px; /*Size for buttons in overlays*/
-  --dp-month-year-row-height: 35px; /*Height of the month-year select row*/
-  --dp-month-year-row-button-size: 35px; /*Specific height for the next/previous buttons*/
-  --dp-button-icon-height: 20px; /*Icon sizing in buttons*/
-  --dp-cell-size: 40px; /*Width and height of calendar cell*/
-  --dp-cell-padding: 5px; /*Padding in the cell*/
-  --dp-common-padding: 10px; /*Common padding used*/
-  --dp-input-icon-padding: 35px; /*Padding on the left side of the input if icon is present*/
-  --dp-input-padding: 6px 30px 6px 12px; /*Padding in the input*/
-  --dp-menu-min-width: 260px; /*Adjust the min width of the menu*/
-  --dp-action-buttons-padding: 2px 5px; /*Adjust padding for the action buttons in action row*/
-  --dp-row-margin: 5px 0; /*Adjust the spacing between rows in the calendar*/
-  --dp-calendar-header-cell-padding: 0.5rem; /*Adjust padding in calendar header cells*/
-  --dp-two-calendars-spacing: 10px; /*Space between multiple calendars*/
-  --dp-overlay-col-padding: 3px; /*Padding in the overlay column*/
-  --dp-time-inc-dec-button-size: 32px; /*Sizing for arrow buttons in the time picker*/
-  --dp-menu-padding: 6px 8px; /*Menu padding*/
+  /* Tamaños */
+  --dp-button-height: 35px; /* Tamaño para botones en superposiciones */
+  --dp-month-year-row-height: 35px; /* Altura de la fila de selección de mes-año */
+  --dp-month-year-row-button-size: 35px; /* Altura específica para los botones siguiente/anterior */
+  --dp-button-icon-height: 20px; /* Tamaño de icono en botones */
+  --dp-cell-size: 40px; /* Ancho y alto de la celda del calendario */
+  --dp-cell-padding: 5px; /* Relleno en la celda */
+  --dp-common-padding: 10px; /* Relleno común utilizado */
+  --dp-input-icon-padding: 35px; /* Relleno en el lado izquierdo del campo de entrada si hay un icono presente */
+  --dp-input-padding: 6px 30px 6px 12px; /* Relleno en el campo de entrada */
+  --dp-menu-min-width: 345px; /* Ajustar el ancho mínimo del menú */
+  --dp-action-buttons-padding: 2px 5px; /* Ajustar el relleno para los botones de acción en la fila de acciones */
+  --dp-row-margin: 5px 0; /* Ajustar el espacio entre filas en el calendario */
+  --dp-calendar-header-cell-padding: 0.5rem; /* Ajustar el relleno en las celdas de encabezado del calendario */
+  --dp-two-calendars-spacing: 10px; /* Espacio entre varios calendarios */
+  --dp-overlay-col-padding: 3px; /* Relleno en la columna de superposición */
+  --dp-time-inc-dec-button-size: 32px; /* Tamaño para botones de incremento/decremento en el selector de tiempo */
+  --dp-menu-padding: 6px 8px; /* Relleno del menú */
 
-  /*Font sizes*/
-  --dp-font-size: 1.0rem; /*Default font-size*/
-  --dp-preview-font-size: 0.8rem; /*Font size of the date preview in the action row*/
-  --dp-time-font-size: 0.8rem; /*Font size in the time picker*/
+  /* Tamaños de fuente */
+  --dp-font-size: 1.0rem; /* Tamaño de fuente predeterminado */
+  --dp-preview-font-size: 0.8rem; /* Tamaño de fuente de la vista previa de la fecha en la fila de acciones */
+  --dp-time-font-size: 0.8rem; /* Tamaño de fuente en el selector de tiempo */
 
-  /*Transitions*/
-  --dp-animation-duration: 0.1s; /*Transition duration*/
+  /* Transiciones */
+  --dp-animation-duration: 0.1s; /* Duración de la transición */
   --dp-menu-appear-transition-timing: cubic-bezier(
     0.4,
     0,
     1,
     1
-  ); /*Timing on menu appear animation*/
-  --dp-transition-timing: ease-out; /*Timing on slide animations*/
+  ); /* Temporización en la animación de aparición del menú */
+  --dp-transition-timing: ease-out; /* Temporización en las animaciones de deslizamiento */
 }
 </style>
